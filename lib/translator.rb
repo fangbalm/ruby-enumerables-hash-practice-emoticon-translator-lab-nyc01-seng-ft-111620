7fc15c0a561aeb6bf248ba72ff1c980ace4ee8de
Lucius VanSlyke <lu.vanslyke@gmail.com>
2020-10-30 06:19:57 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,29 @@
 # require modules here
+require 'yaml'
+require 'pry'
 
-def load_library
+def load_library(file_path)
   # code goes here
+  hash = {}
+  hash["get_emoticon"] = {}
+  hash["meaning"] = {}
+  file = YAML.load_file(file_path)
+
+  file.each do |key, value|
+    hash["get_emote"][value.first] = :english
+    binding.pry
+
+    #   hash = {:english => value[0]}
+    # elsif hash = {:japanese => value[1]}
+    # end
+
+  end
 end
 
-def get_japanese_emoticon
-  # code goes here
-end
-
-def get_english_meaning
-  # code goes here
-end
+# def get_japanese_emoticon
+#   # code goes here
+# end
+#
+# def get_english_meaning
+#   # code goes here
+# end
