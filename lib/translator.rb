6709fe059af0be2a2693b32debcae79d6a86592f
Lucius VanSlyke <lu.vanslyke@gmail.com>
2020-10-30 06:47:56 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -8,7 +8,7 @@ def load_library(file_path)
   file = YAML.load_file(file_path)
 
   file.each do |key, values|
-    values.each do 
+    values.each do
     hash2 = {:english => values[0], :japanese => values[1]}
   end
     hash[key] = hash2
