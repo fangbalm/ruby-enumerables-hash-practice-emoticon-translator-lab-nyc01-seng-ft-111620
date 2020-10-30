74f5ae7c218cf478639733c2db5e29889f3a0d05
Lucius VanSlyke <lu.vanslyke@gmail.com>
2020-10-30 07:14:53 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -23,7 +23,7 @@ def get_english_meaning(file_path, emoticon)
   emoticons = load_library(file_path)
   # code goes here
   emoticons.each do |key, values|
-
+    # if(emoticon) == values[:japanese]
 
     binding.pry
   end
