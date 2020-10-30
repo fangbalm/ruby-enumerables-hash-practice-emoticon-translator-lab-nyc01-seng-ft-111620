140126d85810787735ac40ed3dcde782618fba88
Lucius VanSlyke <lu.vanslyke@gmail.com>
2020-10-30 07:31:37 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -22,7 +22,7 @@ def get_japanese_emoticon(file_path, emoticon)
       return values[:japanese]
     end
   end
-  # code goes here
+  return "Sorry, that emoticon was not found"
 end
 #
 def get_english_meaning(file_path, emoticon)
