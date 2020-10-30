d17dd34cd75cdeb4e434ec6f7031105a041dbcc0
Lucius VanSlyke <lu.vanslyke@gmail.com>
2020-10-30 07:02:35 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -15,10 +15,10 @@ def load_library(file_path)
   return hash
 end
 
-# def get_japanese_emoticon
-#   # code goes here
-# end
+def get_japanese_emoticon
+  # code goes here
+end
 #
-# def get_english_meaning
-#   # code goes here
-# end
+def get_english_meaning
+  # code goes here
+end
